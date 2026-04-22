095c8b1d4b6c637efa05b316598f64efb2721a5c
Realhand robotics <sherwin@realhand.com>
2026-04-22 02:05:44 -0400

    Add files via upload

--- src/realhand/hand/__init__.py
+++ src/realhand/hand/__init__.py
@@ -1,6 +1,7 @@
 from .l6 import L6
+from .l20 import L20
 from .l20lite import L20lite
 from .l25 import L25
 from .o6 import O6
 
-__all__ = ["L6", "O6", "L20lite", "L25"]
+__all__ = ["L6", "O6", "L20", "L20lite", "L25"]
