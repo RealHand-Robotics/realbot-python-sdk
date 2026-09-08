644acb5848f53a87f3ab2101671912dfc66b6312
hnab88 <hatef@realhand.com>
2026-09-08 15:30:18 -0700

    Restore A7 arm support

--- src/realhand/arm/__init__.py
+++ src/realhand/arm/__init__.py
@@ -1,5 +1,6 @@
+from .a7 import A7
 from .a7_lite import A7lite
 from .common import ControlMode, Pose, State, WayPoint
 from .p7 import P7
 
-__all__ = ["A7lite", "P7", "Pose", "ControlMode", "WayPoint", "State"]
+__all__ = ["A7lite", "A7", "P7", "Pose", "ControlMode", "WayPoint", "State"]
